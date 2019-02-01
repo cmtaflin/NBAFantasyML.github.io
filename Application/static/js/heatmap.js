837b1a056e44e0df42af2a3a7a04e83622c1d2aa
Plybuf <matthew.woodmansee@gmail.com>
2019-01-31 21:14:00 -0500

    heatmap js work is done in draft form

--- Application/static/js/heatmap.js
+++ Application/static/js/heatmap.js
@@ -2,53 +2,79 @@
 
 // load the API key for mapbox:
 var apiKey = API_KEY;
-
-// add the basemaps
-var grayScaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-    maxZoom: 15,
-    minZoom: 2,
-    accessToken: API_KEY
-});
-
-var imageMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-  attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
-  maxZoom: 18,
-  id: "mapbox.streets-satellite",
-  accessToken: API_KEY
-});
-
-// create the map object 
 var map = L.map("map", {
     center: [
         37.48, -98.75
     ],
-    zoom: 3,
-    layers: [grayScaleMap, imageMap]
+    zoom: 3
 });
 
+// add the basemaps
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+    maxZoom: 15,
+    id: "mapbox.light",
+    minZoom: 2,
+    accessToken: API_KEY
+}).addTo(map);
+
+// var imageMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+//   attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
+//   maxZoom: 18,
+//   id: "mapbox.streets-satellite",
+//   accessToken: API_KEY
+// });
+
+// create the map object 
+// var map = L.map("map", {
+//     center: [
+//         37.48, -98.75
+//     ],
+//     zoom: 3
+// });
+
 // Add the tile layer to the map.
-grayScaleMap.addTo(map);
+// grayScaleMap.addTo(map);
 
 // create layer for the NBA team locations
 var TeamLoc = new L.LayerGroup();
 
 // create the basemap group
-var baseMaps = {
-    GrayScale: grayScaleMap,
-    Satellite: imagemap
-};
+// var baseMaps = {
+//     GrayScale: grayScaleMap,
+//     Satellite: imagemap
+// };
 
 // URL for api from flask
-var location_path = "../../templates/heatmap_data";
-
+var location_path = "/heatmap_data";
+console.log(location_path);
 d3.json(location_path, function(response) {
-    console.log(response);
+    console.log(response.LATITUDE);
+    // var TeamVenue = [];
+
+    function getRadius(population) {
+        return population * .001;
+    }
+
+    // var styleInfo = {
+    //         opacity:1,
+    //         fillColor: "Red",
+    //         radius: getRadius(response[i].POPULATION)
+    // };
 
     for (var i = 0; i <response.length; i++) {
-        L.marker([LATITUDE, LONGITUDE]).addTo(map);
+        var styleInfo = {
+            opacity:1,
+            fillColor: "Red",
+            radius: getRadius(response[i].POPULATION)
+    };
+        L.circleMarker([response[i].LATITUDE, response[i].LONGITUDE], styleInfo).addTo(TeamLoc);
 
+        console.log(response[i].LATITUDE);
     }
+
 });
+
+TeamLoc.addTo(map);
 
 // create legend for map
